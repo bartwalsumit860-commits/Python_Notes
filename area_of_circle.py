fb3c18314b0596021b3525e57a5992e2a0e59880
sumit <bartwalsumit860@gmail.com>
2026-02-11 07:52:16 +0530

    solve some more basic problems using python

---
+++ area_of_circle.py
@@ -0,0 +1,4 @@
+radius = float(input("enter radius of the circle:"))
+area = 3.14 *(radius**2)
+
+print(f"area of the circle with radius {radius} is {area:.4f}")
---
+++ power_of_number.py
@@ -0,0 +1,7 @@
+n = int(input("Enter a number"))
+
+p = int(input("Enter the power of number"))
+
+ans = (n**p)
+
+print(f"{n}^{p} = {ans}")
